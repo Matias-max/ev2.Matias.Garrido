874a8b0d67a5a0ec9b7302a6c5aa88275930d4d0
Matias-max <garridomatias284@gmail.com>
2020-10-13 17:12:41 -0300

    evaluacion 2

---
+++ Modules/parte1.js
@@ -0,0 +1,51 @@
+export default{
+
+    puntajes: [{alumno:"Pablo Rojas",puntaje:9,resultado:"BUENO"}], 
+    Data: `
+        <h1>Autor: Matias Garrido</h1>
+        alumno<br/>
+        <input id="alumno" type="text"><br/>
+        <select id="puntaje">
+            <option value="$0"></option>
+            <option value="$1"></option>
+            <option value="$2"></option>
+            <option value="$3"></option>
+            <option value="$4"></option>
+            <option value="$5"></option>
+            <option value="$6"></option>
+            <option value="$7"></option>
+            <option value="$8"></option>
+            <option value="$9"></option>
+            <option value="$10"></option>
+
+            
+
+        </select><br/>
+        <button>registrar<button><br>
+
+        <div id="salida" div>
+
+    `,
+    cargar: function(){
+        var salida= document.getElementById(salida);
+        var tabla = html`,
+        <table>: 
+
+        alumno<br/>
+
+        puntaje<br/>
+
+        resultado<br/>
+
+
+        </table>
+
+
+        
+        `,
+    
+
+}();
+
+tabla+= </table>
+salida
