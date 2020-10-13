42519f8cbc54dd38e3e22c92a01d4584b7b08f27
Matias-max <garridomatias284@gmail.com>
2020-10-13 17:51:34 -0300

    evaluacion 2

--- js/Index.js
+++ js/Index.js
@@ -1,3 +1,11 @@
 import parte1 from "./Modules/parte1.js";
 
 var caja = document.getElementById("caja");
+
+document.getElementById("parte1").addEventListener("click"),() =>{
+    caja.innerHTML=parte1.data;
+};
+
+document.getElementById("part2").addEventListener("click"),() =>{
+
+};
--- js/Modules/parte1.js
+++ js/Modules/parte1.js
@@ -6,20 +6,20 @@ export default{
         alumno<br/>
         <input id="alumno" type="text"><br/>
         <select id="puntaje">
-            <option value="$0"></option>
-            <option value="$1"></option>
-            <option value="$2"></option>
-            <option value="$3"></option>
-            <option value="$4"></option>
-            <option value="$5"></option>
-            <option value="$6"></option>
-            <option value="$7"></option>
-            <option value="$8"></option>
-            <option value="$9"></option>
-            <option value="$10"></option>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
         </select><br/>
+        <br/><br/><br/>
         <button>registrar<button><br>
-
         <div id="salida" div>
 
     `,
